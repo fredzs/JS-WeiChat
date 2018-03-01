588992101e120efc45cb3dc4123f4e4dd731d5e1
fredzs <fredzhangsheng@gmail.com>
2018-03-01 23:25:40 +0800

    123

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ App({
     request_url: 'https://fredirox.com/api/',
     local_url: 'https://127.0.0.1:5001/api/',
     test_url: 'https://fredirox.com/test/api/',
-    running_mode: "local_testing"
+    running_mode: "developing"
   },
   get_url: function() {
     var running_mode = this.globalData.running_mode
